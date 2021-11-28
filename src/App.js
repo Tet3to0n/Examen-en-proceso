da50eadf1aee0c6854b2aff35a8340073ed22fd2
Joshua <joshuaherrera@gmail.com>
2021-11-28 13:10:59 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const BASE_URL = "https://www.breakingbadapi.com/api/";
+
+export async function envAPI(endpoint) {
+  try {
+    const response = await axios({
+      url: `${BASE_URL}${endpoint}`,
+      method: `GET`,
+    });
+    return response;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 
 function App() {
-  return (
+  const response = async (endp) => envAPI(endp);
+  const [dataRe, setdataRe] = useState(null);
+  
+  
+
+useEffect(() => {
+   async function loadapi() {
+      const resp = await response("characters");
+    const data = resp?.data;  
+    setdataRe(data);
+    //console.log(resp)
+   }
+   loadapi();
+   return function cleanup() {
+   loadapi();
+   }
+   }, []);
+
+   console.log (dataRe !== null ? dataRe : "es null");
+
+
+
+   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {dataRe !== null ? (
+          <div className="grilla">
+            {dataRe.map((item) => {
+              console.log(item);
+              return (
+                <div className="Card">
+                  <h1 className="h1nombre">{item.name}</h1>
+                  <img
+                    className="avatar"
+                    alt="imagen personaje"
+                    src={item.img}
+                  />
+                  <div className="div1">
+                    <p className="datosP">{item.birthday}</p>
+                    <h2 className="datosP">{item.status}</h2>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <img alt="logo" src={logo} />
+        )}
       </header>
     </div>
   );
